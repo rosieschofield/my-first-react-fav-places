5a58c08b457eb9248df06e5ac59c5735afba5805
Rosie Schofield <rschofield00@gmail.com>
2023-06-28 15:59:01 +0100

    changed footer link

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,7 +3,7 @@ import "./styles.css";
 function Footer(): JSX.Element {
     return (
         <footer className='footer'>
-            <a className = "aStyle" href="http://localhost:3000/">return to top</a>
+            <a className = "aStyle" href="https://nimble-gecko-a6d11d.netlify.app">return to top</a>
         </footer>
     );
 }
