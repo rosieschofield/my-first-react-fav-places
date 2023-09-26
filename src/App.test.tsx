24d644da0ff558c1e683bcf0c4aa1495d97d7cf9
Rosie Schofield <rschofield00@gmail.com>
2023-09-26 15:59:31 +0100

    push latest changes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders greeting", () => {
+test("renders return to top", () => {
   render(<App />);
-  const soughtElement = screen.getByText(/Hello, World!/i);
+  const soughtElement = screen.getByText("Tropical Paradise");
   expect(soughtElement).toBeInTheDocument();
 });
--- src/places.tsx
+++ src/places.tsx
@@ -26,7 +26,7 @@ function Places(props: PlacesProps): JSX.Element {
         </a>
         )
       </h3>
-      <p className = "description">{props.description}</p>
+      <p className="description">{props.description}</p>
     </div>
   );
 }
